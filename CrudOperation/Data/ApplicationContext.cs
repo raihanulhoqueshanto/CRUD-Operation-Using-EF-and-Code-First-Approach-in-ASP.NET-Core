ab1b15c9b61d9f92183c969bbced397a1ee8a811
raihanulhoqueshanto <raihanul.hoque.shanto01@gmail.com>
2023-02-03 00:08:36 +0600

    Project Creation and Database setup done

---
+++ CrudOperation/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using CrudOperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperation.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CrudOperation/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudOperation.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Number { get; set; }
+        public string Email { get; set; }
+    }
+}
